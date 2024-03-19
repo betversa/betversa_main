1c234be7c9c216f79c7d1bb47017256a2cd540c1
betversa <163474908+betversa@users.noreply.github.com>
2024-03-19 01:37:53 +0000

    continue updating odds

--- public/frontend.js
+++ public/frontend.js
@@ -17,6 +17,58 @@ window.onclick = function(event) {
     }
 }
 
+// Dropdown functionality
+var dropdowns = document.getElementsByClassName("dropdown-content");
+var i;
+
+for (i = 0; i < dropdowns.length; i++) {
+    dropdowns[i].parentNode.addEventListener("click", function() {
+        this.classList.toggle("show");
+        var dropdownContent = this.nextElementSibling;
+        if (dropdownContent.style.display === "block") {
+            dropdownContent.style.display = "none";
+        } else {
+            dropdownContent.style.display = "block";
+        }
+    });
+}
+
+// Close the dropdown if the user clicks outside of it
+window.onclick = function(event) {
+    if (!event.target.matches('.header-link')) {
+        var dropdowns = document.getElementsByClassName("dropdown-content");
+        var i;
+        for (i = 0; i < dropdowns.length; i++) {
+            var openDropdown = dropdowns[i];
+            if (openDropdown.style.display === "block") {
+                openDropdown.style.display = "none";
+            }
+        }
+    }
+}
+
+// Get all the elements with the class "header-link-container"
+var dropdowns = document.getElementsByClassName("header-link-container");
+
+// Loop through the elements and add an event listener to each
+for (var i = 0; i < dropdowns.length; i++) {
+    dropdowns[i].addEventListener('click', function(event) {
+        // Prevent the default action of the link
+        event.preventDefault();
+
+        // Get the dropdown content of the clicked element
+        var dropdownContent = this.getElementsByClassName("dropdown-content")[0];
+
+        // Toggle the display of the dropdown content
+        if (dropdownContent.style.display === "block") {
+            dropdownContent.style.display = "none";
+        } else {
+            dropdownContent.style.display = "block";
+        }
+    });
+}
+
+
 // Function to fetch live odds and display them in the odds container
 function fetchOdds() {
     fetch('/api/odds')
@@ -34,7 +86,6 @@ function fetchOdds() {
                         awayRow.innerHTML = `
                             <div class="team">${event.away_team}</div>
                             <div class="odds">${market.outcomes.find(outcome => outcome.name === event.away_team).price}</div>
-                            <div
                         `;
                         oddsContainer.appendChild(awayRow);
 
@@ -62,7 +113,3 @@ fetchOdds();
 
 // Set an interval to update the odds every 5 minutes (300000 milliseconds)
 setInterval(fetchOdds, 300000);
-
-
-
-
